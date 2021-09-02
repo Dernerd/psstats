6633a904fd65ceb2f16decfd46d1d8267bb61c16
Dernerd <dern3rd@outlook.at>
2021-09-02 21:49:18 +0200

    Über

--- classes/WpPsstats/Admin/views/info_shared.php
+++ classes/WpPsstats/Admin/views/info_shared.php
@@ -11,16 +11,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // if accessed directly
 }
 ?>
-<h1><?php esc_html_e( 'About', 'psstats' ); ?> <?php psstats_header_icon(true);  ?> </h1>
+<h1><?php esc_html_e( 'Über', 'psstats' ); ?> <?php psstats_header_icon(true);  ?> </h1>
 
 <p>
 	<?php
 	echo sprintf(
 		__(
-			'%1$sPsstats Analytics%2$s is the most powerful
-    analytics platform for WordPress, designed for your success. It is our mission to help you grow
-    your business while giving you %3$sfull control over your data%4$s. All
-    data is stored in your WordPress. You own the data, nobody else.',
+			'%1$sPS STATS%2$s ist eine einfache aber mächtige
+    Analytics Platform für WordPress, basierend auf dem Quellcode von Matomo. Es ist unsere Mission, 
+	Dir beim Wachstum Deines Unternehmens zu helfen und Dir gleichzeitig %3$volle Kontrolle über Deine Daten zu geben%4$s.
+    Alle Daten werden nur in Deinem WordPress gespeichert. Du besitzt die Daten, niemand sonst.',
 			'psstats'
 		),
 		'<a target="_blank" rel="noreferrer noopener" href="https://n3rds.work">',
@@ -31,9 +31,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	?>
 </p>
 <ul class="psstats-list">
-	<li><?php esc_html_e( '100% data ownership, no one else can see your data', 'psstats' ); ?></li>
-	<li><?php esc_html_e( 'Powerful web analytics for WordPress', 'psstats' ); ?></li>
-	<li><?php esc_html_e( 'Superb user privacy protection', 'psstats' ); ?></li>
-	<li><?php esc_html_e( 'No data limits or sampling whatsoever', 'psstats' ); ?></li>
-	<li><?php esc_html_e( 'Easy installation and configuration', 'psstats' ); ?></li>
+	<li><?php esc_html_e( '100 % Dateneigentum, niemand sonst kann Deine Daten sehen', 'psstats' ); ?></li>
+	<li><?php esc_html_e( 'Leistungsstarke Webanalyse für WordPress', 'psstats' ); ?></li>
+	<li><?php esc_html_e( 'Hervorragender Schutz der Privatsphäre der Benutzer', 'psstats' ); ?></li>
+	<li><?php esc_html_e( 'Keine Datenlimits oder Sampling überhaupt', 'psstats' ); ?></li>
+	<li><?php esc_html_e( 'Einfache Installation und Konfigurationn', 'psstats' ); ?></li>
+	<li><?php esc_html_e( 'Endlich verständliche Datenanalyse auf Deutsch', 'psstats' ); ?></li>
 </ul>
